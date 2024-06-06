428476aa9da221c3bc5d31b3132887d961401c58
ignaciohernandez <nachoher93@gmail.com>
2024-06-06 18:22:41 -0300

    json para proyecto de nosotros

---
+++ js/FetchNosotros.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let original = document.querySelector("#Integrantes");
+    let contenedor = document.querySelector("#Contenedor");
+
+    let botonAgregar = document.querySelector("#Agregar");
+    let botonQuitar = document.querySelector("#Quitar");
+
+    let referencia = original.cloneNode(true);
+    original.remove(); 
+
+  
+    let currentIndex = 0;
+    let integrantesData = [];
+
+    // Cargar los datos del JSON al cargar la página
+    fetch("https://github.com/damiansuarez1979/TP1_CaC_Grupo2/tree/main/data.json")
+        .then(response => response.json())
+        .then(data => {
+            // Guardar los datos recibidos
+            integrantesData = data.integrantes;
+            console.log("Datos recibidos:", integrantesData); // Ver los datos completos
+        })
+        .catch(error => console.log("Algo salio mal " + error));
+
+    function AgregarArticulo() {
+        // Verificar que haya más integrantes por agregar
+        if (currentIndex < integrantesData.length) {
+            let integrante = integrantesData[currentIndex];
+
+            console.log("Nombre:", integrante.nombre);
+            console.log("Apellido:", integrante.apellido);
+            console.log("Foto Perfil:", integrante.foto_perfil);
+
+            let nuevaPersona = referencia.cloneNode(true);
+            nuevaPersona.querySelector("img").src = integrante.foto_perfil;
+            nuevaPersona.querySelector("img").alt = "Foto Integrante";
+            nuevaPersona.querySelector(".nombre").innerHTML = integrante.nombre + " " + integrante.apellido;
+            nuevaPersona.querySelector(".edad").innerHTML = integrante.edad + " años";
+            nuevaPersona.querySelector(".residencia").innerHTML = integrante.residencia;
+
+            contenedor.appendChild(nuevaPersona);
+
+            // Incrementar el índice del integrante actual
+            currentIndex++;
+        } else {
+            console.log("No hay más integrantes para agregar.");
+        }
+    }
+
+    function QuitarArticulo() {
+        if (contenedor.childElementCount > 0) {
+            contenedor.removeChild(contenedor.lastChild);
+            // Decrementar el índice del integrante actual para poder agregarlo de nuevo si es necesario
+            if (currentIndex > 0) {
+                currentIndex--;
+            }
+        }
+    }
+
+    // Eventos
+    botonAgregar.addEventListener("click", function() {
+        AgregarArticulo();
+    });
+
+    botonQuitar.addEventListener("click", function() {
+        QuitarArticulo();
+    });
+});
