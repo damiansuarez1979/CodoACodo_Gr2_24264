7cd813e1b7ce72aef9975ebc4d122060b0331508
ignaciohernandez <nachoher93@gmail.com>
2024-06-06 18:36:42 -0300

    actualizacion

--- js/FetchNosotros.js
+++ js/FetchNosotros.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let original = document.querySelector("#Integrantes");
+    let original = document.querySelector("#Desarrolladores");
     let contenedor = document.querySelector("#Contenedor");
 
     let botonAgregar = document.querySelector("#Agregar");
@@ -13,44 +13,44 @@ document.addEventListener("DOMContentLoaded", function() {
     let integrantesData = [];
 
     // Cargar los datos del JSON al cargar la página
-    fetch("https://github.com/damiansuarez1979/TP1_CaC_Grupo2/tree/main/data.json")
+    fetch("https://github.com/damiansuarez1979/CodoACodo_Gr2_24264/blob/main/data.json")
         .then(response => response.json())
         .then(data => {
             // Guardar los datos recibidos
-            integrantesData = data.integrantes;
-            console.log("Datos recibidos:", integrantesData); // Ver los datos completos
+            desarroladoresData = data.desarroladores;
+            console.log("Datos recibidos:", desarroladoresData); // Ver los datos completos
         })
         .catch(error => console.log("Algo salio mal " + error));
 
     function AgregarArticulo() {
-        // Verificar que haya más integrantes por agregar
-        if (currentIndex < integrantesData.length) {
-            let integrante = integrantesData[currentIndex];
+        // Verificar que haya más desarroladores por agregar
+        if (currentIndex < desarroladoresData.length) {
+            let desarrolador = desarroladoresData[currentIndex];
 
-            console.log("Nombre:", integrante.nombre);
-            console.log("Apellido:", integrante.apellido);
-            console.log("Foto Perfil:", integrante.foto_perfil);
+            console.log("Nombre:", desarrolador.nombre);
+            console.log("Apellido:", desarrolador.apellido);
+            console.log("Foto Perfil:", desarrolador.foto_perfil);
 
             let nuevaPersona = referencia.cloneNode(true);
-            nuevaPersona.querySelector("img").src = integrante.foto_perfil;
-            nuevaPersona.querySelector("img").alt = "Foto Integrante";
-            nuevaPersona.querySelector(".nombre").innerHTML = integrante.nombre + " " + integrante.apellido;
-            nuevaPersona.querySelector(".edad").innerHTML = integrante.edad + " años";
+            nuevaPersona.querySelector("img").src = desarrolador.foto_perfil;
+            nuevaPersona.querySelector("img").alt = "Foto Desarrollador";
+            nuevaPersona.querySelector(".nombre").innerHTML = desarrolado.nombre + " " + desarrolador.apellido;
+            nuevaPersona.querySelector(".edad").innerHTML = desarrolador.edad + " años";
             nuevaPersona.querySelector(".residencia").innerHTML = integrante.residencia;
 
             contenedor.appendChild(nuevaPersona);
 
-            // Incrementar el índice del integrante actual
+            // Incrementar el índice del desarrollador actual
             currentIndex++;
         } else {
-            console.log("No hay más integrantes para agregar.");
+            console.log("No hay más Desarrolladores para agregar.");
         }
     }
 
     function QuitarArticulo() {
         if (contenedor.childElementCount > 0) {
             contenedor.removeChild(contenedor.lastChild);
-            // Decrementar el índice del integrante actual para poder agregarlo de nuevo si es necesario
+            // Decrementar el índice del desarrollador actual para poder agregarlo de nuevo si es necesario
             if (currentIndex > 0) {
                 currentIndex--;
             }
