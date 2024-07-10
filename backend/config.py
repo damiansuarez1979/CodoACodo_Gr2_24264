44f6ca67d686a3f3744820e3356b38815541ed80
ignaciohernandez <nachoher93@gmail.com>
2024-07-10 12:01:11 -0300

    cambiodb

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://dbuser:yt1mrcOEbdBwEqKNfpw1yQKVGJokNJTg@dpg-cq7a1b5ds78s738tl3kg-a.oregon-postgres.render.com/tienda_de_bicicletas'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://dbuser:yt1mrcOEbdBwEqKNfpw1yQKVGJokNJTg@dpg-cq7a1b5ds78s738tl3kg-a/tienda_de_bicicletas'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
