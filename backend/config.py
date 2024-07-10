4fadc53c29ff53379ee31406705319ac9b1cebd3
ignaciohernandez <nachoher93@gmail.com>
2024-07-10 10:25:54 -0300

    cambios de DB

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username:password@localhost/mydatabase'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://dbuser:password@localhost/tienda_de_biciletas'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
